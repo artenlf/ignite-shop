4df56b550d4cf004242ff6eeb7cc4d2e1b7fa21f
artenlf <felipe.arten@gmail.com>
2023-02-06 23:23:46 -0300

    feat: created products page structure and styles

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,3 +1,4 @@
+import { ImageContainer, ProductContainer, ProductDetails } from "@/src/styles/pages/product"
 import { useRouter } from "next/router"
 
 
@@ -5,6 +6,18 @@ export default function Product() {
   const { query } = useRouter()
 
   return (
-    <h1>Product: {JSON.stringify(query)}</h1>
+    <ProductContainer>
+      <ImageContainer>
+      </ImageContainer>
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,00</span>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, totam non voluptas enim ad sequi sapiente quam ut quis modi reprehenderit doloribus. Voluptas iusto temporibus, sit nam tenetur provident architecto.</p>
+        <button>
+          Comprar agora
+        </button>
+      </ProductDetails>
+    </ProductContainer>
   )
 }
